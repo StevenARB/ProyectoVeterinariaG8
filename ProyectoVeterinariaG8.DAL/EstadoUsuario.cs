d09dd1c249ee02af0acd9e56200be620c9e5fcf6
Manuel Lopez <manuelslop95@gmail.com>
2024-02-23 18:47:49 -0600

    Tablas: Usuario - EstadoUsuario - Rol

---
+++ ProyectoVeterinariaG8.DAL/EstadoUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVeterinariaG8.DAL
+{
+    [Table("EstadoUsuario")]
+    public class EstadoUsuario
+    {
+        [Key]
+        [DisplayName("Código de Estado")]
+        public int CodigoEstado { get; set; }
+
+        [Required]
+        [DisplayName("Descripción")]
+        public string Descripcion { get; set; }
+    }
+}
---
+++ ProyectoVeterinariaG8.DAL/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVeterinariaG8.DAL
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        [DisplayName("Código de usuario")]
+        public int CodigoUsuario { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+
+        [Required]
+        [DisplayName("Contraseña")]
+        public string Contrasenna { get; set; }
+
+        [Required]
+        [ForeignKey("Rol")]
+        public int CodigoRol { get; set; }
+
+        [Required]
+        public string Imagen { get; set; }
+
+        [Required]
+        [ForeignKey("EstadoUsuario")]
+        public int CodigoEstado { get; set; }
+
+        [Required]
+        [DisplayName("Fecha de creación")]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
