3c5bd57b68eec40ba679f77fc952f01556dfeff8
JayMS01 <jayfabrizioms@gmail.com>
2024-02-24 16:09:35 -0600

    Jay

---
+++ ProyectoVeterinariaG8.DAL/Citas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVeterinariaG8.DAL
+{
+    [Table("Citas")]
+    public class Citas
+    {
+        [Key]
+        public int CitaId { get; set; }
+
+        [ForeignKey("Mascotas")]
+        public int MascotaId { get; set; }
+
+        [Required]
+        [DisplayName("Fecha y Hora de la Cita")]
+        public DateTime FechayHora { get; set; }
+
+        [Required]
+        [ForeignKey("Usuario")]
+        [DisplayName("Veterinario Principal")]
+        public int CodigoUsuario { get; set; }
+
+        [Required]
+        [ForeignKey("Usuario")]
+        [DisplayName("Veterinario Secundario")]
+         public int CodigoUsuario2 { get; set; }
+
+        [Required]
+        [DisplayName("Descripción de la Cita")]
+        [MaxLength(500)]
+        public string DescripcionCita { get; set; }
+
+        [DisplayName("Diagnóstico de la Cita")]
+        [MaxLength(200)]
+        public string DiagnosticoCita { get; set; }
+
+
+        [ForeignKey("Medicamento")]
+        [DisplayName("Medicamentos")]
+        public int MedicamentoId { get; set; }
+
+        [Required]
+        [ForeignKey("EstadoCita")]
+        public int EstadoCitaId { get; set; }
+
+
+        public Usuario? Usuario { get; set; }
+
+        public ICollection<Usuario> Usuarios { get; set; } = new List<Usuario>();
+    }
+}
--- ProyectoVeterinariaG8.DAL/VeterinariaContext.cs
+++ ProyectoVeterinariaG8.DAL/VeterinariaContext.cs
@@ -1,7 +1,24 @@
-﻿namespace ProyectoVeterinariaG8.DAL
-{
-    public class VeterinariaContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace ProyectoVeterinariaG8.DAL
+{
+    public class VeterinariaContext : DbContext
+    {
+        public VeterinariaContext() { }
+
+        public VeterinariaContext(DbContextOptions<VeterinariaContext>)
+
+
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+
+        public virtual DbSet<Citas> Citas { get; set; }
+
+        public virtual DbSet<Medicamento> Medicamento { get; set;}
+
+        public virtual DbSet<EstadoCita> EstadoCitas { get; set; }
     }
 }
