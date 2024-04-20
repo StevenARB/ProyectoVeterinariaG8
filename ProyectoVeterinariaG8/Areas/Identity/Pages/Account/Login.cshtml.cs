99ffb512d56bbc22d90d80964fe62782629d15b1
StevenRB <retanasteven8@gmail.com>
2024-04-20 09:15:49 -0600

    Administrador Editar Usuario

--- ProyectoVeterinariaG8/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ProyectoVeterinariaG8/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace ProyectoVeterinariaG8.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -115,6 +117,9 @@ namespace ProyectoVeterinariaG8.Areas.Identity.Pages.Account
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    user.FechaUltimaConexion = DateTime.Now;
+                    await _userManager.UpdateAsync(user);
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
--- ProyectoVeterinariaG8/Controllers/AdminController.cs
+++ ProyectoVeterinariaG8/Controllers/AdminController.cs
@@ -12,14 +12,16 @@ namespace ProyectoVeterinariaG8.Controllers
     public class AdminController : Controller
     {
         private readonly AuthContext _authContext;
+        private readonly VeterinariaContext _veterinariaContext;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
 
-        public AdminController(AuthContext authContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IUserStore<ApplicationUser> userStore)
+        public AdminController(AuthContext authContext, VeterinariaContext veterinariaContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IUserStore<ApplicationUser> userStore)
         {
             _authContext = authContext;
+            _veterinariaContext = veterinariaContext;
             _userManager = userManager;
             _roleManager = roleManager;
             _userStore = userStore;
@@ -34,16 +36,28 @@ namespace ProyectoVeterinariaG8.Controllers
         [Authorize(Roles = "Administrador")]
         public IActionResult CreateUser()
         {
+            ViewData["Estados"] = new SelectList(_veterinariaContext.EstadosUsuario, "EstadoId", "Descripcion");
             ViewData["Roles"] = new SelectList(_roleManager.Roles, "Id", "NormalizedName");
             return View();
         }
 
         [HttpPost]
         [Authorize(Roles = "Administrador")]
-        public async Task<IActionResult> CreateUser(AdminCreateUserViewModel userModel) 
+        public async Task<IActionResult> CreateUser(AdminCreateUserViewModel userModel, IFormFile imagen)
         {
             if (ModelState.IsValid)
             {
+                byte[]? imagenVariable = null;
+
+                if (imagen != null && imagen.Length > 0)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        await imagen.CopyToAsync(memoryStream);
+                        imagenVariable = memoryStream.ToArray();
+                    }
+                }
+
                 var user = new ApplicationUser();
 
                 await _userStore.SetUserNameAsync(user, userModel.Email, CancellationToken.None);
@@ -51,9 +65,9 @@ namespace ProyectoVeterinariaG8.Controllers
                 user.Nombre = userModel.Nombre;
                 user.PrimerApellido = userModel.PrimerApellido;
                 user.SegundoApellido = userModel.SegundoApellido;
-                user.Imagen = userModel.Imagen;
+                user.Imagen = imagenVariable;
                 user.FechaUltimaConexion = userModel.FechaUltimaConexion;
-                user.EstadoUsuarioId = 1;
+                user.EstadoUsuarioId = userModel.EstadoUsuarioId;
                 var result = await _userManager.CreateAsync(user, userModel.Password);
 
                 if (result.Succeeded)
@@ -64,10 +78,91 @@ namespace ProyectoVeterinariaG8.Controllers
 
                     return RedirectToAction("Index", "Home");
                 }
-                }
-
+            }
+            ViewData["Estados"] = new SelectList(_veterinariaContext.EstadosUsuario, "EstadoId", "Descripcion");
             ViewData["Roles"] = new SelectList(_roleManager.Roles, "Id", "NormalizedName");
             return View(userModel);
+        }
+
+        public async Task<IActionResult> EditUser(string? id)
+        {
+            if (id == null || _userManager.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var model = new AdminEditUserViewModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Nombre = user.Nombre,
+                PrimerApellido = user.PrimerApellido,
+                SegundoApellido = user.SegundoApellido,
+                Imagen = user.Imagen,
+                EstadoUsuarioId = user.EstadoUsuarioId
+            };
+
+            ViewData["Estados"] = new SelectList(_veterinariaContext.EstadosUsuario, "EstadoId", "Descripcion");
+            ViewData["Roles"] = new SelectList(_roleManager.Roles, "Id", "NormalizedName");
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditUser(string? id, AdminEditUserViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (id != model.Id || user == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+
+                user.Email = model.Email;
+                user.Nombre = model.Nombre;
+                user.PrimerApellido = model.PrimerApellido;
+                user.SegundoApellido = model.SegundoApellido;
+                user.Imagen = model.Imagen;
+                user.EstadoUsuarioId = model.EstadoUsuarioId;
+
+                if (model.Password != null && model.Password == model.ConfirmPassword)
+                {
+                    var changePassword = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.Password);
+
+                    if (changePassword.Succeeded)
+                    {
+                        TempData["UserPasswordUpdatedMessage"] = "La contraseña se ha actualizado correctamente.";
+                    }
+                }
+
+                var result = await _userManager.UpdateAsync(user);
+
+                if (result.Succeeded) 
+                {
+                    TempData["UserUpdatedMessage"] = "El usuario se ha actualizado correctamente.";
+                    //return RedirectToAction(nameof(Index));
+                }
+                ViewData["Estados"] = new SelectList(_veterinariaContext.EstadosUsuario, "EstadoId", "Descripcion", model.EstadoUsuarioId);
+                ViewData["Roles"] = new SelectList(_roleManager.Roles, "Id", "NormalizedName");
+            }
+
+            ViewData["Estados"] = new SelectList(_veterinariaContext.EstadosUsuario, "EstadoId", "Descripcion");
+            ViewData["Roles"] = new SelectList(_roleManager.Roles, "Id", "NormalizedName");
+            return View(model);
+        }
+
+        private bool UserExists(string id)
+        {
+            return (_authContext.Users?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
